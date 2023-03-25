b2a707d83752279580eee30bce0d3d35ddd01eb4
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-03-25 15:24:10 +0200

    2 task is done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const newElement = document.querySelector('ul#ingredients');
+
+ingredients.forEach((ingredient)=>{
+  const li = document.createElement('li');
+  li.classList.add('item');
+  li.textContent = ingredient;
+  newElement.appendChild(li);
+})
+
