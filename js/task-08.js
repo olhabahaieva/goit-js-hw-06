e38d2bdc6340a7ccbba46c7ec9c6d65b094dc454
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-03-26 12:21:36 +0300

    task 8 is done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+//General form
+const form = document.querySelector("form.login-form");
+
+//Event listener
+form.addEventListener("submit", onSubmit);
+
+//Function
+function onSubmit(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+  if (!form.elements.email.value || !form.elements.password.value) {
+    alert("All fields must be filled out");
+  } else {
+    const data = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(data);
+  } 
+  // Reset the form
+  form.reset();
+}
+
+
 
