aa00223e4814e41cffdcd544d764147a4efe6b28
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-03-26 21:39:28 +0300

    8 task is corrected

--- js/task-08.js
+++ js/task-08.js
@@ -10,13 +10,15 @@ function onSubmit(event) {
   const { email, password } = event.currentTarget.elements;
   if (!email.value || !password.value) {
     alert("All fields must be filled out");
-    // Reset the form
-    form.reset();
+   
   } else {
     const data = {
       email: email.value,
       password: password.value,
     };
     console.log(data);
+    
+     // Reset the form
+     form.reset();
   }
 }
