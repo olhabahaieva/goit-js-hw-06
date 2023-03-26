e726f60145ab20fe5f350fa42276de0ad2981aa2
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-03-26 12:45:03 +0300

    task 9 is done

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,20 @@
+// Button Element
+const button = document.querySelector('button.change-color');
+
+// Span Text
+const text = document.querySelector('span.color');
+
+//Event Listener
+button.addEventListener("click", getRandomHexColor);
+
+//Function
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  const color = Math.floor(Math.random() * 16777215)
+  .toString(16)
+  .padStart(6, 0);
+  // return `#${Math.floor(Math.random() * 16777215)
+  //   .toString(16)
+  //   .padStart(6, 0)}`;
+    document.body.style.backgroundColor = '#' + color;
+    text.textContent = '#' + color;
 }
